d3a9fadc3e2a420a7f9fb666a50dba73f644cbee
Jaume Nogues <jnogues@gmail.com>
2023-07-03 16:39:14 +0200

    Update whitecatN2-abp.ino

--- code/whitecatN2/whitecatN2-abp.ino
+++ code/whitecatN2/whitecatN2-abp.ino
@@ -30,7 +30,7 @@ static osjob_t sendjob;
 
 // Schedule TX every this many seconds (might become longer due to duty
 // cycle limitations).
-const unsigned TX_INTERVAL = 60;//temps entre enviaments
+const unsigned TX_INTERVAL = 60;//temps entre enviaments, necessari si no dorm
 
 // Pin mapping
 #define NSS_GPIO        9
